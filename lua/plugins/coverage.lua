44167aa6d873e72e15760f7da42d5b6bb971040b
Ethan Holz <ethan.holz@thoriumworks.com>
2026-03-18 14:28:46 -0600

    slim down nvim

--- lua/plugins/coverage.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "andythigpen/nvim-coverage",
-        dependencies = { "nvim-lua/plenary.nvim", "EdenEast/nightfox.nvim" },
-        -- lazy = true,
-        ft = { "python", "rust" },
-        config = true,
-    },
-}
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,6 +3,44 @@ return {
         "folke/snacks.nvim",
         opts = {
             input = {},
+            picker = {},
+        },
+        keys = {
+            {
+                "<leader>ff",
+                function()
+                    Snacks.picker.files()
+                end,
+                desc = "Find files",
+            },
+            {
+                "<leader>fg",
+                function()
+                    Snacks.picker.grep()
+                end,
+                desc = "Grep",
+            },
+            {
+                "<leader>s",
+                function()
+                    Snacks.picker.spelling()
+                end,
+                desc = "Spelling",
+            },
+            {
+                "<leader>gr",
+                function()
+                    Snacks.picker.lsp_references()
+                end,
+                desc = "References",
+            },
+            {
+                "<leader>gi",
+                function()
+                    Snacks.picker.lsp_implementations()
+                end,
+                desc = "Implementations",
+            },
         },
     },
 }
