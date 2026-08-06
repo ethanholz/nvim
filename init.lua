b4d3586271c540d114ff2ab2b740b454837a31c9
Ethan Holz <ethan.holz@thoriumworks.com>
2026-08-06 12:55:38 -0600

    refactor: remove nvim-lastplace :(

--- init.lua
+++ init.lua
@@ -49,7 +49,6 @@ vim.pack.add({
     github("Saecki/crates.nvim"),
     github("ziglang/zig.vim"),
     github("EdenEast/nightfox.nvim"),
-    github("ethanholz/nvim-lastplace"),
     github("folke/which-key.nvim"),
     github("folke/snacks.nvim"),
     github("echasnovski/mini.icons"),
@@ -141,7 +140,19 @@ vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
         })
     end,
 })
-require("nvim-lastplace").setup()
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function(args)
+        local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+        local line_count = vim.api.nvim_buf_line_count(args.buf)
+        if mark[1] > 0 and mark[1] <= line_count then
+            vim.api.nvim_win_set_cursor(0, mark)
+            -- defer centering slightly so it's applied after render
+            vim.schedule(function()
+                vim.cmd("normal! zz")
+            end)
+        end
+    end,
+})
 require("which-key").setup()
 require("lualine").setup({
     options = {
